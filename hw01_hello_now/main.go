1992829a3e77ca18a756530d22539a5625f6947e
Konstantin Ivanin <ivanin.k@gmail.com>
2020-04-26 18:57:31 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	t := time.Now()
+	et, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	fmt.Println("current time: ", t.UTC())
+
+	if err == nil {
+		fmt.Println("exact time: ", et.UTC().Round(0))
+	} else {
+		log.Fatalf(err.Error())
+	}
 }
